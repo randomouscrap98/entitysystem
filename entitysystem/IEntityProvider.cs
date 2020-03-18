78702f6093dac4df87370c3a023dc68d7dcfe802
Carlos Sanchez <randomouscrap98@aol.com>
2020-03-18 02:01:55 -0400

    Comment clarifying lastId requirement

--- entitysystem/IEntityProvider.cs
+++ entitysystem/IEntityProvider.cs
@@ -12,6 +12,20 @@ namespace Randomous.EntitySystem
 
         Task WriteAsync<E>(IEnumerable<E> entities) where E : EntityBase;
         Task DeleteAsync<E>(IEnumerable<E> items) where E : EntityBase;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Notice "lastId". You might think this could be generic and ONLY have a filter. No,
+        /// the "lastId" is an optimization so the ENTIRE dang table doesn't have to be searched, because
+        /// EFCore is weird. Keep lastId, this is our standard use case anyway. Filter is just extra.
+        /// </remarks>
+        /// <param name="lastId"></param>
+        /// <param name="maxWait"></param>
+        /// <param name="filter"></param>
+        /// <typeparam name="E"></typeparam>
+        /// <returns></returns>
         Task<List<E>> ListenNewAsync<E>(long lastId, TimeSpan maxWait, Func<E, bool> filter = null) where E : EntityBase;
     }
 }
