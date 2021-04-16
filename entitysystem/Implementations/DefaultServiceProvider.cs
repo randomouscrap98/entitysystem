f09f339500fd178112aafab6764de4d08e6dd9b7
Carlos Sanchez <randomouscrap98@aol.com>
2021-04-15 22:53:12 -0400

    Small cleanup

--- entitysystem/Implementations/DefaultServiceProvider.cs
+++ entitysystem/Implementations/DefaultServiceProvider.cs
@@ -9,13 +9,12 @@ namespace Randomous.EntitySystem.Implementations
         public void AddDefaultServices(IServiceCollection services, Action<DbContextOptionsBuilder> buildContext, Action<DbContext> modifyContext = null)
         {
             services.AddSingleton(new GeneralHelper());
+            services.AddSingleton(typeof(ISignaler<>), typeof(SignalSystem<>)); //No, this is setup for a STANDARD use case which would WANT a single signaller
+            services.AddSingleton<EntityQueryableEfCoreConfig>(); //Just get some defaults in there...
+
             services.AddTransient<IEntitySearcher, EntitySearcher>();
             services.AddTransient<IEntityQueryable, EntityQueryableEfCore>();
-            //services.AddTransient<ISignaler<EntityBase>, SignalSystem<EntityBase>>(); //you may think the signaler needs to be a singleton. Not always... people can add that themselves.
-            services.AddSingleton(typeof(ISignaler<>), typeof(SignalSystem<>)); //No, this is setup for a STANDARD use case which would WANT a single signaller
             services.AddTransient<IEntityProvider, EntityProvider>();
-            services.AddSingleton<EntityQueryableEfCoreConfig>(); //Just get some defaults in there...
-            //services.AddTransient<EntityProviderConfig>();
             services.AddDbContext<BaseEntityContext>(buildContext, ServiceLifetime.Transient, ServiceLifetime.Transient);
             services.AddTransient<DbContext, BaseEntityContext>(s =>
             {
